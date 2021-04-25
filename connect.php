93e8b08ae0dffa89b3033e8c968b49f68ce61a74
KirillGul <50946729+KirillGul@users.noreply.github.com>
2021-04-25 16:27:45 +0300

    Step 1

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+//Устанавливаем доступы к базе данных:
+$host = 'localhost'; //имя хоста, на локальном компьютере это localhost
+$user = 'root'; //имя пользователя, по умолчанию это root
+$password = 'root'; //пароль, по умолчанию пустой
+$db_name = 'test'; //имя базы данных
+//Соединяемся с базой данных используя наши доступы:
+ $link = mysqli_connect($host, $user, $password, $db_name) or die(mysqli_error($link));
+ //Устанавливаем кодировку (не обязательно, но поможет избежать проблем):
+ mysqli_query($link, "SET NAMES 'utf8'");
